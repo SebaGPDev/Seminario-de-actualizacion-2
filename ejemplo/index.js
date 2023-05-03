855990bf9c8ca689814e5cf1ccccd88b12548fce
SebaGPDev <sebakpo120@gmai.com>
2023-05-03 16:48:43 -0300

    to Chance

---
+++ ejemplo/index.js
@@ -0,0 +1,37 @@
+const btn = document.getElementById("btn");
+const model = tf.sequential();
+
+async function loadData() {
+  const response = await fetch("./data.json");
+  const data = await response.json();
+  const xs = tf.tensor(data.datos.entradas, [6, 1]);
+//   const ys = tf.tensor(data.salidas, [6, 1]);
+
+console.log(data)
+
+return { xs, ys };
+
+}
+
+loadData()
+
+
+
+// async function entrenar() {
+//   const data = await loadData();
+
+//   model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+
+//   // Prepare the model for training: Specify the loss and the optimizer.
+//   model.compile({ loss: "meanSquaredError", optimizer: "sgd" });
+
+//   // Train the model using the data.
+//   await model.fit(data.xs, data.ys, { epochs: 250 });
+//   alert("terminó");
+// }
+
+// function predecir() {
+//   const input = document.getElementById("input").value;
+//   const output = model.predict(tf.tensor2d([input], [1, 1])).dataSync()[0];
+//   document.getElementById("micro-out-div").innerText = output;
+// }
